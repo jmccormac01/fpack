d7421de734f9c32d678624f3a1addd5996078d92
jmccormac01 <jmccormac001@gmail.com>
2016-01-28 07:47:48 +0000

    fixing layout and adding if name==main

--- fpack_data.py
+++ fpack_data.py
@@ -32,33 +32,21 @@ if args.n:
 if not args.n:
 	DRYRUN = 0
 
-def GoIn(place):
-	
-	#if place == "original":
-	#	print "Not entering original data directory..."
-	#	return 0
-	
+def GoIn(place):	
 	os.chdir(place)
-	
 	h=os.getcwd()
 	print h
-	
-	
 	t=os.listdir('.')
-	GetSummary(t)
-	
+	GetSummary(t)	
 	for i in range(0,len(t)):
 		if os.path.isdir(t[i]) == True:
 			print "%s is a directory..." % (t[i])
 			GoIn(t[i])
-	
 	os.chdir('../')
-
 	return 0
 
 
 def GetSummary(t):
-	
 	for i in range(0,len(t)):
 		if ".fit" in t[i] and t[i].endswith(".gz"):
 			print "\tZippedFit(s): %s..." % (t[i])
@@ -66,55 +54,46 @@ def GetSummary(t):
 			if DRYRUN == 0:
 				os.system('gunzip %s' % t[i])
 				os.system('fpack -Y -D %s' % t[i].split('.gz')[0])
-			
-			
 		if ".fts" in t[i] and t[i].endswith(".gz"):
 			print "\tZippedFts: %s..." % (t[i])
 			print "\t\tUnzipping %s --> fpacking %s..." % (t[i],t[i].split('.gz')[0])
 			if DRYRUN == 0:
 				os.system('gunzip %s' % t[i])
 				os.system('fpack -Y -D %s' % t[i].split('.gz')[0])
-		
 		if ".fit" in t[i] and t[i].endswith(".fz"):
 			print "\tFpackedFit(s): %s..." % (t[i])
 			print "\t\tFile %s is fine, not touching..." % (t[i])
 			continue
-			
 		if ".fts" in t[i] and t[i].endswith(".fz"):
 			print "\tFpackedFit(s): %s..." % (t[i])
 			print "\t\tFile %s is fine, not touching..." % (t[i])
 			continue
-			
 		if ".fit" in t[i] and t[i].endswith(".gz") == False:
 			print "\tFit(s): %s..." % (t[i])
 			print "\t\tfpacking %s..." % (t[i])
 			if DRYRUN == 0:
 				os.system('fpack -Y -D %s' % t[i])
-			
 		if ".fts" in t[i] and t[i].endswith(".gz") == False:
 			print "\tFts: %s..." % (t[i])
 			print "\t\tfpacking %s..." % (t[i])	
 			if DRYRUN == 0:
 				os.system('fpack -Y -D %s' % t[i])
-		
 		if t[i].endswith(".gz") and ".fit" not in t[i] and ".fts" not in t[i]:
 			print "\tZipped: %s..." % (t[i])
 			print "\t\tFile %s is fine, not touching..." % (t[i])
-		
-	
 	return 0
 
-####################
-####### MAIN #######
-####################
 
-here=os.getcwd()
-t=os.listdir(here)
+def main():
+	here=os.getcwd()
+	t=os.listdir(here)
+	GetSummary(t)
+	for i in range(0,len(t)):
+		if os.path.isdir(t[i]) == True:
+			print "%s is a directory..." % (t[i])
+			GoIn(t[i])
 
-GetSummary(t)
+if __name__ == '__main__':
+	main()
 
-for i in range(0,len(t)):
-	if os.path.isdir(t[i]) == True:
-		print "%s is a directory..." % (t[i])
-		GoIn(t[i])
 
